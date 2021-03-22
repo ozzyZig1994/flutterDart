1f385c048ea7db682cd2aabb3e6782f051ea10b2
unknown <adio_him@hotmail.com>
2021-03-22 15:32:35 -0600

    Capitulo acerca de componentes...

---
+++ componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: ListView(
+        //children: _crearItems(),
+        children: _crearItemsCorta (),
+      ),
+    );
+  }
+
+  // ignore: unused_element
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    var widgets = opciones.map((item) {
+      return Column(
+        children: [
+          ListTile (
+            title: Text (item + '!'),
+            subtitle: Text ('Subtitulo'),
+            leading: Icon (Icons.ac_unit),
+            trailing: Icon (Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider ()
+        ],
+      );
+    }).toList ();
+
+    return widgets;
+  }
+}
