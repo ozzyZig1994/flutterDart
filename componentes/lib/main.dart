5ef6ac9ee7f795fd7386740849322d20a157f06a
unknown <adio_him@hotmail.com>
2021-03-23 13:35:55 -0600

    Se agregaron rutas a las vistas Alert y Avatar

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage (),
+      //home: HomePage (),
+      initialRoute: '/',
+      routes: getAplicaciones (),
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage (),
+        );
+      },
     );
   }
 }
