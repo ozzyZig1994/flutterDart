0f1534f9287cfdcee633ec94b550cf5208abe48a
unknown <adio_him@hotmail.com>
2021-06-04 16:46:41 -0500

    awsome dialog para aviso de autenticacion erronea

--- kc_app/lib/src/utils/utils.dart
+++ kc_app/lib/src/utils/utils.dart
@@ -1,36 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:awesome_dialog/awesome_dialog.dart';
+
 void mostrarAlerta(BuildContext context, String mensaje) {
-  showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-            title: Text('Credenciales incorrectas'),
-            content: Text(mensaje),
-            actions: <Widget>[
-              TextButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: Text('Ok'))
-            ],
-          ));
+  AwesomeDialog(
+    context: context,
+    dialogType: DialogType.ERROR,
+    borderSide: BorderSide(color: Colors.green, width: 2),
+    buttonsBorderRadius: BorderRadius.all(Radius.circular(2)),
+    headerAnimationLoop: false,
+    animType: AnimType.SCALE,
+    title: 'ERROR',
+    desc: mensaje,
+    showCloseIcon: true,
+  )..show();
 }
 
 void mostrarLoader(BuildContext context) {
-    showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Image.asset('cloud.gif'),
-              ],
-            ),
-          );
-        });
-  }
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset('cloud.gif'),
+            ],
+          ),
+        );
+      });
+}
 
 void ocultarLoader(BuildContext context) {
   Navigator.pop(context);
