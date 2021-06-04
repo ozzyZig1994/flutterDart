43656c11e883fd10510815200a1fb6f741dc3257
unknown <adio_him@hotmail.com>
2021-06-04 13:40:37 -0500

    Mas correcciones

--- kc_app/lib/main.dart
+++ kc_app/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:kc_app/src/pages/cultivo_page.dart';
-import 'package:kc_app/src/pages/galeria_page.dart';
-import 'package:kc_app/src/pages/pdf_view_page.dart';
 
 import 'package:kc_app/src/preferencias_usuario/preferencias_usuario.dart';
 import 'package:kc_app/src/providers/provider.dart';
 import 'package:kc_app/src/pages/home_page.dart';
 import 'package:kc_app/src/pages/login_page.dart';
+import 'package:kc_app/src/pages/pdf_view_page.dart';
+import 'package:kc_app/src/pages/cultivo_page.dart';
+import 'package:kc_app/src/pages/galeria_cultivo_page.dart';
 
 void main() async {
   final prefs = new PreferenciasUsuario();
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
           HomePage.routeName: (BuildContext context) => HomePage(),
           CultivoPage.routeName : (BuildContext context) => CultivoPage(),
           PdfViewPage.routeName : (BuildContext context) => PdfViewPage(),
+          GaleriaCultivoPage.routeName : (BuildContext context) => GaleriaCultivoPage()
         },
         theme: ThemeData(primaryColor: Colors.deepPurple),
       ),
--- kc_app/lib/src/pages/galeria_cultivo_page.dart
+++ kc_app/lib/src/pages/galeria_cultivo_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class GaleriaCultivoPage extends StatelessWidget {
+  static final String routeName = 'galeria_cultivo';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Galeria'),
+      ),
     );
   }
 }
