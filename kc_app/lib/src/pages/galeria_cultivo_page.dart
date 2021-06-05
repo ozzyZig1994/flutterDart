9e3cd1168b274d356b099d4fa108b405ae47fc48
Abraham Josúe Galván Torres <adio_him@hotmail.com>
2021-06-05 16:18:23 -0500

    diseño de galeria de cultivo, termindado

--- kc_app/lib/src/pages/galeria_cultivo_page.dart
+++ kc_app/lib/src/pages/galeria_cultivo_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:kc_app/src/widgets/card_swiper_widget.dart';
+
 class GaleriaCultivoPage extends StatelessWidget {
   static final String routeName = 'galeria_cultivo';
 
@@ -12,6 +14,13 @@ class GaleriaCultivoPage extends StatelessWidget {
         title: Text('Galeria de $cultivo'),
         centerTitle: true,
       ),
+      body: Center(
+        child: _swiperTarjetas(),
+      ),
     );
+  }
+
+  Widget _swiperTarjetas() {
+    return CardSwiper();
   }
 }
---
+++ kc_app/lib/src/widgets/card_swiper_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class CardSwiper extends StatelessWidget {
+  //final List<dynamic> fotos;
+
+  //CardSwiper({@required this.fotos});
+
+  @override
+  Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+    return Container(
+      child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.network(
+                  "https://via.placeholder.com/350x150",
+                  fit: BoxFit.fill,
+                ));
+          },
+          itemCount: 3,
+          layout: SwiperLayout.STACK,
+          itemWidth: _screenSize.width * 0.9,
+          itemHeight: _screenSize.height * 0.6),
+    );
+  }
+}
