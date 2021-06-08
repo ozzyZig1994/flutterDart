e820bf8c59444edaa7d873f121507d8aae00fd72
Abraham Josúe Galván Torres <adio_him@hotmail.com>
2021-06-08 14:08:33 -0500

    galeria implementada

--- kc_app/lib/src/widgets/card_swiper_widget.dart
+++ kc_app/lib/src/widgets/card_swiper_widget.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:kc_app/src/providers/galeria_provider.dart';
 
 class CardSwiper extends StatelessWidget {
   final String cultivo;
+  final galeriaProvider = new GaleriaProvider();
 
   CardSwiper({@required this.cultivo});
 
   @override
   Widget build(BuildContext context) {
     final _screenSize = MediaQuery.of(context).size;
-    return Container(
-      child: Swiper(
-          itemBuilder: (BuildContext context, int index) {
-            return ClipRRect(
-                borderRadius: BorderRadius.circular(20.0),
-                child: Image.network(
-                  "https://via.placeholder.com/350x150",
-                  fit: BoxFit.fill,
-                ));
-          },
-          itemCount: 3,
-          layout: SwiperLayout.STACK,
-          itemWidth: _screenSize.width * 0.9,
-          itemHeight: _screenSize.height * 0.6),
+    return FutureBuilder(
+      future: galeriaProvider.buscaCultivo(cultivo),
+      builder: (context, snapshot) {
+        if ((snapshot.connectionState == ConnectionState.done) &
+            snapshot.hasData) {
+          final galeria = snapshot.data;
+          return Container(
+            child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image.network(
+                        galeria['galeria'][index]['ruta'],
+                        fit: BoxFit.fill,
+                      ));
+                },
+                itemCount: galeria['galeria'].length,
+                layout: SwiperLayout.STACK,
+                itemWidth: _screenSize.width * 0.9,
+                itemHeight: _screenSize.height * 0.6),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 }
