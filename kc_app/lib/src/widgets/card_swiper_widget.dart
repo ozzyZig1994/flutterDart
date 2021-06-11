4e11511fe05b3c387647437d8a29115f70ed2bb6
unknown <adio_him@hotmail.com>
2021-06-11 12:49:08 -0500

    solucion de bugs y optimizacion

--- kc_app/lib/src/widgets/card_swiper_widget.dart
+++ kc_app/lib/src/widgets/card_swiper_widget.dart
@@ -11,40 +11,64 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _screenSize = MediaQuery.of(context).size;
     return FutureBuilder(
       future: galeriaProvider.buscaCultivo(cultivo),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasData) {
-            final galeria = snapshot.data;
-            int numFotos =
-                (galeria['galeria'] != null) ? galeria['galeria'].length : 0;
-            return Container(
-              child: Swiper(
-                  itemBuilder: (BuildContext context, int index) {
-                    return ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: FadeInImage(
-                          placeholder: AssetImage('no-image.jpg'),
-                          image:
-                              NetworkImage(galeria['galeria'][index]['ruta']),
-                          fit: BoxFit.fill,
-                        ));
-                  },
-                  itemCount: numFotos,
-                  layout: SwiperLayout.STACK,
-                  itemWidth: _screenSize.width * 0.9,
-                  itemHeight: _screenSize.height * 0.6),
-            );
-          } else {
-            final galeria = snapshot.data;
-            return Center(child: Text(galeria['message']));
-          }
+        if ((snapshot.connectionState == ConnectionState.done) &
+            snapshot.hasData) {
+          if (snapshot.data['galeria'] != null) {
+            if (snapshot.data['galeria'].length > 1)
+              return _swiper(context, snapshot);
+            else
+              return _unaSolaImagen(context, snapshot);
+          } else
+            return _alerta(snapshot);
         } else {
           return CircularProgressIndicator();
         }
       },
     );
+  }
+
+  Widget _swiper(BuildContext context, AsyncSnapshot snapshot) {
+    final _screenSize = MediaQuery.of(context).size;
+    final galeria = snapshot.data;
+    int numFotos = galeria['galeria'].length;
+
+    return Container(
+      child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: FadeInImage(
+                  placeholder: AssetImage('no-image.jpg'),
+                  image: NetworkImage(galeria['galeria'][index]['ruta']),
+                  fit: BoxFit.fill,
+                ));
+          },
+          itemCount: numFotos,
+          layout: SwiperLayout.STACK,
+          itemWidth: _screenSize.width * 0.9,
+          itemHeight: _screenSize.height * 0.6),
+    );
+  }
+
+  Widget _alerta(AsyncSnapshot snapshot) {
+    return Center(child: Text(snapshot.data['message']));
+  }
+
+  Widget _unaSolaImagen(BuildContext context, AsyncSnapshot snapshot) {
+    final _screenSize = MediaQuery.of(context).size;
+    final galeria = snapshot.data;
+
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: FadeInImage(
+          width: _screenSize.width * 0.9,
+          height: _screenSize.height * 0.6,
+          placeholder: AssetImage('no-image.jpg'),
+          image: NetworkImage(galeria['galeria'][0]['ruta']),
+          fit: BoxFit.fill,
+        ));
   }
 }
